3fe4f18c0dae5d0c6f25a143a6b83e85f16d7969
Marco Bueno <marquinhosbueno59@gmail.com>
2023-12-30 01:20:33 -0300

    att

--- js/script.js
+++ js/script.js
@@ -4,3 +4,17 @@ function toggleMenu () {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+let prevScrollpos = window.pageYOffset;
+
+window.onscroll = function () {
+    let currentScrollPos = window.pageYOffset;
+
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementById("desktop-nav").style.top = "0";
+    } else {
+        document.getElementById("desktop-nav").style.top = "-70px"; // Altura da Navbar
+    }
+
+    prevScrollpos = currentScrollPos;
+};
