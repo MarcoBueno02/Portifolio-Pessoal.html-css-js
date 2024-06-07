f2e7ecd78725d33b6ec56de3834b5cf49aa822c8
Marco Bueno <marquinhosbueno59@gmail.com>
2024-06-07 18:47:21 -0300

    att

--- js/script.js
+++ js/script.js
@@ -34,22 +34,40 @@ function toggleMenu () {
       }
   
       prevScrollPos = currentScrollPos;
+  };function toggleMenu () {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+  }
+  
+  let prevScrollpos = window.pageYOffset;
+  
+  window.onscroll = function () {
+      let currentScrollPos = window.pageYOffset;
+  
+      if (prevScrollpos > currentScrollPos) {
+          document.getElementById("desktop-nav").style.top = "0";
+      } else {
+          document.getElementById("desktop-nav").style.top = "-70px"; // Altura da Navbar
+      }
+  
+      prevScrollpos = currentScrollPos;
   };
   
-  // Função para capitalizar a primeira letra de cada frase e após pontos finais
-  function capitalizeSentences(text) {
-      return text.replace(/(^\w{1})|(\.\s*\w{1})/g, match => match.toUpperCase());
-  }
+  let prevScrollPos = window.pageYOffset;
+  let hamburgerNav = document.getElementById("hamburger-nav");
   
-  // Função para aplicar a capitalização aos elementos desejados
-  function applyCapitalization() {
-      const elements = document.querySelectorAll('.nav-list li, #profile, section, #sobre, #projetos, #contato, footer');
-      
-      elements.forEach(element => {
-          element.innerHTML = capitalizeSentences(element.innerHTML);
-      });
-  }
+  window.onscroll = function () {
+      let currentScrollPos = window.pageYOffset;
   
-  // Aplicar a capitalização quando o DOM estiver totalmente carregado
-  document.addEventListener("DOMContentLoaded", applyCapitalization);
+      if (prevScrollPos > currentScrollPos) {
+          // Rolar para cima
+          hamburgerNav.style.top = "0";
+      } else {
+          // Rolar para baixo
+          hamburgerNav.style.top = `-${hamburgerNav.clientHeight}px`;
+      }
   
+      prevScrollPos = currentScrollPos;
+  };
