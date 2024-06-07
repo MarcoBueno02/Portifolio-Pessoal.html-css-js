75dcb35f2908c9a192056d5bb098dcf217eca987
Marco Bueno <marquinhosbueno59@gmail.com>
2024-06-07 18:52:03 -0300

    att

--- js/script.js
+++ js/script.js
@@ -1,73 +1,37 @@
 function toggleMenu () {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
-  }
-  
-  let prevScrollpos = window.pageYOffset;
-  
-  window.onscroll = function () {
-      let currentScrollPos = window.pageYOffset;
-  
-      if (prevScrollpos > currentScrollPos) {
-          document.getElementById("desktop-nav").style.top = "0";
-      } else {
-          document.getElementById("desktop-nav").style.top = "-70px"; // Altura da Navbar
-      }
-  
-      prevScrollpos = currentScrollPos;
-  };
-  
-  let prevScrollPos = window.pageYOffset;
-  let hamburgerNav = document.getElementById("hamburger-nav");
-  
-  window.onscroll = function () {
-      let currentScrollPos = window.pageYOffset;
-  
-      if (prevScrollPos > currentScrollPos) {
-          // Rolar para cima
-          hamburgerNav.style.top = "0";
-      } else {
-          // Rolar para baixo
-          hamburgerNav.style.top = `-${hamburgerNav.clientHeight}px`;
-      }
-  
-      prevScrollPos = currentScrollPos;
-  };function toggleMenu () {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
-  }
-  
-  let prevScrollpos = window.pageYOffset;
-  
-  window.onscroll = function () {
-      let currentScrollPos = window.pageYOffset;
-  
-      if (prevScrollpos > currentScrollPos) {
-          document.getElementById("desktop-nav").style.top = "0";
-      } else {
-          document.getElementById("desktop-nav").style.top = "-70px"; // Altura da Navbar
-      }
-  
-      prevScrollpos = currentScrollPos;
-  };
-  
-  let prevScrollPos = window.pageYOffset;
-  let hamburgerNav = document.getElementById("hamburger-nav");
-  
-  window.onscroll = function () {
-      let currentScrollPos = window.pageYOffset;
-  
-      if (prevScrollPos > currentScrollPos) {
-          // Rolar para cima
-          hamburgerNav.style.top = "0";
-      } else {
-          // Rolar para baixo
-          hamburgerNav.style.top = `-${hamburgerNav.clientHeight}px`;
-      }
-  
-      prevScrollPos = currentScrollPos;
-  };
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+
+let prevScrollpos = window.pageYOffset;
+
+window.onscroll = function () {
+    let currentScrollPos = window.pageYOffset;
+
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementById("desktop-nav").style.top = "0";
+    } else {
+        document.getElementById("desktop-nav").style.top = "-70px"; // Altura da Navbar
+    }
+
+    prevScrollpos = currentScrollPos;
+};
+
+let prevScrollPos = window.pageYOffset;
+let hamburgerNav = document.getElementById("hamburger-nav");
+
+window.onscroll = function () {
+    let currentScrollPos = window.pageYOffset;
+
+    if (prevScrollPos > currentScrollPos) {
+        // Rolar para cima
+        hamburgerNav.style.top = "0";
+    } else {
+        // Rolar para baixo
+        hamburgerNav.style.top = `-${hamburgerNav.clientHeight}px`;
+    }
+
+    prevScrollPos = currentScrollPos;
+};
